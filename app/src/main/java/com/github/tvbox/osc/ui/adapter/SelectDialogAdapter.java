999e74aa06d6d64b9cb7786fa8631c563744145e
watson1982 <112817572+watson1982@users.noreply.github.com>
2023-12-02 14:27:33 +0800

    SelectDialogAdapter

--- app/src/main/java/com/github/tvbox/osc/ui/adapter/SelectDialogAdapter.java
+++ app/src/main/java/com/github/tvbox/osc/ui/adapter/SelectDialogAdapter.java
@@ -90,15 +90,9 @@ public class SelectDialogAdapter<T> extends ListAdapter<T, SelectDialogAdapter.S
     public void onBindViewHolder(@NonNull @NotNull SelectDialogAdapter.SelectViewHolder holder, @SuppressLint("RecyclerView") int position) {
         T value = data.get(position);
         String name = dialogInterface.getDisplay(value);
-        TextView view = holder.itemView.findViewById(R.id.tvName);
-        if (!muteCheck && position == select) {
-            view.setTextColor(0xff02f8e1);
-            view .setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
-        }else {
-            view.setTextColor(Color.WHITE);
-            view .setTypeface(Typeface.defaultFromStyle(Typeface.NORMAL));
-        }
-        view.setText(name);
+        if (!muteCheck && position == select)
+            name = "√ " + name;
+        ((TextView) holder.itemView.findViewById(R.id.tvName)).setText(name);
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
