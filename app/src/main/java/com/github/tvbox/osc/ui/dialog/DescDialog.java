444839ff0053972b2b3e9132cf60874efd37f0c3
watson1982 <112817572+watson1982@users.noreply.github.com>
2024-01-13 19:24:17 +0800

    DescDialog

--- app/src/main/java/com/github/tvbox/osc/ui/dialog/DescDialog.java
+++ app/src/main/java/com/github/tvbox/osc/ui/dialog/DescDialog.java
@@ -21,7 +21,7 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PushDialog extends BaseDialog {
+public class DescDialog extends BaseDialog {
 
     private EditText etAddr;
     private EditText etPort;
